0a94f47d53555e5b226551775931e248d9d7f69f
yehyunee <into1023@naver.com>
2018-10-04 22:36:08 +0900

    빠진부분

---
+++ app/src/main/java/com/example/yehyunee/tagapplication/viewholder/MainViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.yehyunee.tagapplication.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class MainViewHolder extends RecyclerView.ViewHolder {
+    public MainViewHolder(View itemView) {
+        super(itemView);
+    }
+}
